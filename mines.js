89e578351210da64c5ba350ede2d11a9088015b9
n3vermin9 <89288662119s@mail.ru>
2024-10-25 19:44:17 +0300

    update

--- mines.js
+++ mines.js
@@ -17,6 +17,10 @@ let setMineCount = 3
 let coef = 0.8;
 let multiple = [1.3, 1.6, 2, 2.68, 2.9, 3.1, 3.8, 4.3, 5, 5.6, 6, 6.9, 7.3, 8, 8.8, 9.3, 10, 11.2, 12.5, 13.3, 14.5, 15.4, 16.7, 18, 20]
 
+let tileCount = 0;
+let isntBomb;
+let isWin = false;
+
 let currentMultiply = document.createElement('div')
 currentMultiply.className = 'greenMultiply'
 
@@ -48,6 +52,17 @@ init()
 
 playBtn.addEventListener('click', () => {
 if (mainInput.innerText != '$000' && localStorage.getItem('balance') > 0 && localStorage.getItem('balance') >= inputBet) {
+    // reset before new game
+    tileCount = 0
+    isWin = false
+    isntBomb = false
+
+    tile.forEach(element => {
+      element.classList.remove('clicked', 'thisBomb', 'notBomb')
+      element.innerText = ''
+      element.style.WebkitAnimationPlayState = "paused";
+    });
+
     bigBombCount.style.display = 'none';
     mainBottom.style.visibility = 'hidden';
     takeBtn.style.visibility = 'visible'
@@ -58,6 +73,7 @@ if (mainInput.innerText != '$000' && localStorage.getItem('balance') > 0 && loca
       let balanceRes = balanceValue -= inputBet
       localStorage.setItem('balance', `${balanceRes}`)
       balance.innerText = localStorage.getItem('balance')
+
     }
 }
 });
@@ -81,20 +97,19 @@ btnMinusMines.addEventListener('click', () => {
     currentMultiply.innerText = `x${multiple[setMineCount - 3]}`
   }
 })
-let tileCount = 0;
-let isntBomb;
-let isWin = false;
+
 let winChance = Math.random()
 
 
 tile.forEach(element => {
   element.addEventListener('click', () => {
+    console.log(tileCount)
     winChance = Math.random()
     if (element.classList.contains('clicked')) return;
     element.style.WebkitAnimationPlayState = "running";
     if (tileCount == 24 - setMineCount) {
       tile.forEach(element => {
-        isWin = true;
+        isWin = true;   
         tile.forEach(leftElem => {
           if (!(leftElem.classList.contains('clicked'))) {
             leftElem.classList.add('thisBomb')
@@ -109,12 +124,14 @@ tile.forEach(element => {
         element.innerHTML = '<svg  xmlns="http://www.w3.org/2000/svg"  width="60"  height="60"  viewBox="0 0 24 24"  fill="currentColor"  class="tile-diamond"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 4a1 1 0 0 1 .783 .378l.074 .108l3 5a1 1 0 0 1 -.032 1.078l-.08 .103l-8.53 9.533a1.7 1.7 0 0 1 -1.215 .51c-.4 0 -.785 -.14 -1.11 -.417l-.135 -.126l-8.5 -9.5a1 1 0 0 1 -.172 -1.067l.06 -.115l3.013 -5.022l.064 -.09a.982 .982 0 0 1 .155 -.154l.089 -.064l.088 -.05l.05 -.023l.06 -.025l.109 -.032l.112 -.02l.117 -.005h12zm-8.886 3.943a1 1 0 0 0 -1.371 .343l-.6 1l-.06 .116a1 1 0 0 0 .177 1.07l2 2.2l.09 .088a1 1 0 0 0 1.323 -.02l.087 -.09a1 1 0 0 0 -.02 -1.323l-1.501 -1.65l.218 -.363l.055 -.103a1 1 0 0 0 -.398 -1.268z" /></svg>'
         isntBomb = false;
         tileCount++;
+        currentMultiply.innerText = `x${multiple[setMineCount++ - 3]}`
       } else if (winChance > coef && isWin == false) {
         element.classList.add('thisBomb', 'clicked');
         tile.forEach(leftElem => {
           if (!(leftElem.classList.contains('clicked'))) {   
             element.innerHTML = '<svg  xmlns="http://www.w3.org/2000/svg"  width="60"  height="60"  viewBox="0 0 24 24"  fill="currentColor"  class="tile-bomb"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14.499 3.996a2.2 2.2 0 0 1 1.556 .645l3.302 3.301a2.2 2.2 0 0 1 0 3.113l-.567 .567l.043 .192a8.5 8.5 0 0 1 -3.732 8.83l-.23 .144a8.5 8.5 0 1 1 -2.687 -15.623l.192 .042l.567 -.566a2.2 2.2 0 0 1 1.362 -.636zm-4.499 5.004a4 4 0 0 0 -4 4a1 1 0 0 0 2 0a2 2 0 0 1 2 -2a1 1 0 0 0 0 -2z" /><path d="M21 2a1 1 0 0 1 .117 1.993l-.117 .007h-1c0 .83 -.302 1.629 -.846 2.25l-.154 .163l-1.293 1.293a1 1 0 0 1 -1.497 -1.32l.083 -.094l1.293 -1.292c.232 -.232 .375 -.537 .407 -.86l.007 -.14a2 2 0 0 1 1.85 -1.995l.15 -.005h1z" /></svg>'
             leftElem.classList.add('thisBomb', 'clicked')
+
           }
         });
         isntBomb = true;
@@ -132,9 +149,17 @@ tile.forEach(element => {
 
 
 takeBtn.addEventListener('click', ()=> {
-  if (tileCount == 0) {
+  if (tileCount == 0 || isntBomb == false) {
    return 
   }
+  localStorage.setItem('balance', balanceValue + inputBet * currentMultiply.innerText.substring(1))
+  balance.innerHTML = localStorage.getItem('balance')
+  bigBombCount.style.display = 'flex';
+  mainBottom.style.visibility = 'visible';
+  takeBtn.style.visibility = 'hidden'
+  tile.forEach(element => {
+    element.style.display = 'none';
+  });
 })
 
 
